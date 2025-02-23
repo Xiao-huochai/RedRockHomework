a46617dad7c3ad149f9b7b34a1602bf56c4229fa
Xiao-huochai <chai437437@gmail.com>
2025-02-23 11:23:07 +0800

    打包测试

---
+++ Winter vacation/neteasecloudmusicapi-main/webpack.config.js
@@ -0,0 +1,60 @@
+// const path = require('path');
+
+// module.exports = {
+//     mode: 'development' || 'production',
+//     entry: "./js/search.js",
+//     output: {
+//         filename: "bundle.js",
+//         path: path.resolve(__dirname, "./dist"),
+//         clean: true
+//     },
+//     module: {
+//         rules: [
+//             {
+//                 test: /\.css$/,
+//                 use: ['style-loader', 'css-loader'],
+//             },
+//             {
+//                 test: /\.js$/,
+//                 use: [
+//                     'babel-loader'
+//                 ],
+//                 exclude: '/node_modules',
+//             }
+//         ],
+//     },
+// }
+
+const path = require("path");
+const { merge } = require("webpack-merge");
+const devConfig = require("./webpack.dev.js");
+const prodConfig = require("./webpack.prod.js");
+const commonConfig = {
+    entry: "./js/search.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        clean: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.js$/,
+                use: ["babel-loader"],
+                exclude: "/node_modules/",
+            },
+        ],
+    },
+};
+
+//  将配置导出为函数
+module.exports = (env) => {
+    if (env && env.production) {
+        return merge(commonConfig, prodConfig);
+    } else {
+        return merge(commonConfig, devConfig);
+    }
+};
